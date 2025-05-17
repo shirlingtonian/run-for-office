a3146f02aecf0e32511f599da57eaf5f3a757344
shirlingtonian <benrivera3@gmail.com>
2025-05-16 22:15:58 -0400

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
-import RunForOfficeGame from "./game.js";
-
-ReactDOM.render(React.createElement(RunForOfficeGame), document.getElementById("root"));
