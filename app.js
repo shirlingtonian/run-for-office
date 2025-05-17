4b8df6eac62ae6f93cf1557f7ab7f0b4ae6eb976
shirlingtonian <benrivera3@gmail.com>
2025-05-16 22:04:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom";
+import RunForOfficeGame from "./game.js";
+
+ReactDOM.render(React.createElement(RunForOfficeGame), document.getElementById("root"));
